f293914938e1cb49da38d5eea5826ac33aff71f2
Duong Anh Vu <duonganhvu494@gmail.com>
2025-05-28 21:11:29 +0700

    push lab3

---
+++ LAB3/src/main/java/org/example/RoundedPanel.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundedPanel extends JPanel {
+    private final int cornerRadius;
+
+    public RoundedPanel(int radius) {
+        this.cornerRadius = radius;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.setColor(getBackground());
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), cornerRadius, cornerRadius);
+
+        g2.dispose();
+        super.paintComponent(g);
+    }
+}
